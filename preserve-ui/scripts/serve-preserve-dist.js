c67229c93dcf6668c9fc62ff105b97eee66b965c
Tony Finn <tony@tonyfinn.com>
2021-01-03 16:31:09 +0000

    CI: Use express from dev dependencies

---
+++ preserve-ui/scripts/serve-preserve-dist.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const process = require('process');
+const app = express();
+
+const preserve_path = process.env.PRESERVE_PATH || '../../dist';
+const port = parseInt(process.env.PRESERVE_PORT, 10) || 5678;
+
+app.use('/', express.static(preserve_path));
+app.listen(port, () => {
+    const portStr = port !== 80 ? `:${port}` : '';
+    console.log(`Open http://localhost${portStr}/ in a web browser for the app`);
+});
+
+process.on('SIGTERM', () => {
+    process.exit(0);
+});
